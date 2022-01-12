45ca2ac99c6961ce51ae10fcde3fc7df4268f6e6
pumba-dev <dudu32769144@gmail.com>
2022-01-12 04:20:19 -0300

    Fix Bug in Hover Transition

--- pumba-web-front/src/components/header/MailBtn.vue
+++ pumba-web-front/src/components/header/MailBtn.vue
@@ -6,15 +6,17 @@
 
 <style scoped>
 .btn-text {
-  @apply text-blue text-2xl font-normal
-  hover:text-white-gray;
+  @apply text-2xl font-normal;
 }
 
 .btn-background {
   @apply flex justify-center items-center 
-  w-36 h-12 
+  w-36 h-12 text-blue
   border-solid border-2 border-blue rounded-full
-  hover:bg-blue
   cursor-pointer;
+}
+
+.btn-background:hover {
+  @apply text-white-gray bg-blue;
 }
 </style>
