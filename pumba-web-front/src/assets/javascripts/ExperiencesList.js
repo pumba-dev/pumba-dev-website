ff2b14af2fd94123d22e85dbedcee41e45ea8acd
pumba-dev <dudu32769144@gmail.com>
2022-01-15 23:50:36 -0300

    Src Test

--- pumba-web-front/src/assets/javascripts/ExperiencesList.js
+++ pumba-web-front/src/assets/javascripts/ExperiencesList.js
@@ -2,13 +2,13 @@ export default [
     {
         "title": "Ciência da Computação",
         "desc": "Bacharel no curso de ciência da computação em andamento na Universidade Federal do Piauí - UFPI.      2017 - 2023",
-        "iconSrc": "../../assets/svg/experiences/university-icon.svg",
+        "iconSrc": "src/assets/svg/experiences/university-icon.svg",
         "alt": "Ícone da Universidade",
     },
     {
         "title": "Iniciação Científica",
         "desc": "Pesquisa na área de Redes Ópticas Elásticas com Multiplexação por Divisal Espacial. Foi produzido um artigo ciêntífico que foi apresentação no SBRC 2021.",
-        "iconSrc": "../../assets/svg/experiences/cnpq-icon.svg",
+        "iconSrc": "src/assets/svg/experiences/cnpq-icon.svg",
         "alt": "Ícone do CNPQ",
     },
 ]
