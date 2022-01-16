8ab4ab9356baf94988f08e9a57e538fa03515823
pumba-dev <dudu32769144@gmail.com>
2022-01-16 00:13:23 -0300

    More one try of fix src bug

--- pumba-web-front/src/assets/javascripts/SocialList.js
+++ pumba-web-front/src/assets/javascripts/SocialList.js
@@ -1,32 +1,32 @@
 export default [
     {
         "name": "Twitter",
-        "iconSrc": "../../assets/svg/social/twitter-icon.svg",
+        "iconSrc": "./src/assets/svg/social/twitter-icon.svg",
         "link": "https://www.twitter.com/pumbadev",
     },
     {
         "name": "Facebook",
-        "iconSrc": "../../assets/svg/social/facebook-icon.svg",
+        "iconSrc": "./src/assets/svg/social/facebook-icon.svg",
         "link": "https://www.facebook.com/pumbadev/",
     },
     {
         "name": "Instagram",
-        "iconSrc": "../../assets/svg/social/instagram-icon.svg",
+        "iconSrc": "./src/assets/svg/social/instagram-icon.svg",
         "link": "https://www.instagram.com/strogonothing/",
     },
     {
         "name": "GitHub",
-        "iconSrc": "../../assets/svg/social/github-icon.svg",
+        "iconSrc": "./src/assets/svg/social/github-icon.svg",
         "link": "https://github.com/pumba-dev",
     },
     {
         "name": "Linkedin",
-        "iconSrc": "../../assets/svg/social/linkedin-icon.svg",
+        "iconSrc": "./src/assets/svg/social/linkedin-icon.svg",
         "link": "https://www.linkedin.com/in/pumba-dev/",
     },
     {
         "name": "Email",
-        "iconSrc": "../../assets/svg/social/mail-icon.svg",
+        "iconSrc": "./src/assets/svg/social/mail-icon.svg",
         "link": "mailto:pumbadeveloper@gmail.com",
     },
 ]
