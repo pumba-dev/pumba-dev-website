bdad856ad85a14b2f3174beb14f80f57bdbafe28
pumba-dev <dudu32769144@gmail.com>
2022-01-15 23:44:00 -0300

    Fix Img Src Deploy Bug

--- pumba-web-front/src/assets/javascripts/QualitiesList.js
+++ pumba-web-front/src/assets/javascripts/QualitiesList.js
@@ -2,19 +2,19 @@ export default [
     {
         "title": "Junior",
         "desc": "Com mais de 1 ano estudando para me tornar desenvolvedor full-stack, tento ser hoje um profissional melhor do que fui ontem.",
-        "iconSrc": "/src/assets/svg/qualitys/junior-icon.svg",
+        "iconSrc": "../../assets/svg/qualitys/junior-icon.svg",
         "alt": "Ícone de Desenvolvedor Júnior",
     },
     {
         "title": "Clean Code",
         "desc": "Durante o desenvolvimento, a qualidade do código é a diretriz mais importante. Sempre seguindo as melhores práticas da literatura.",
-        "iconSrc": "/src/assets/svg/qualitys/clean-code-icon.svg",
+        "iconSrc": "../../assets/svg/qualitys/clean-code-icon.svg",
         "alt": "Ícone de Código Limpo",
     },
     {
         "title": "Metodologias Ágeis",
         "desc": "Utilizo metodologias e planejamentos que otimizam o meu fluxo de produção. Seguir o protocolo preparado torna o processo ágil e eficiente.",
-        "iconSrc": "/src/assets/svg/qualitys/agile-soft-icon.svg",
+        "iconSrc": "../../assets/svg/qualitys/agile-soft-icon.svg",
         "alt": "Ícone de Metodologias Ágeis",
     },
 ]
