03a750a019f63d7fb03266164b8af47bf406b7f2
pumba-dev <dudu32769144@gmail.com>
2022-01-19 14:05:22 -0300

    Header Nav Reponsive Fix

--- pumba-web-front/src/components/header/HeaderNavigation.vue
+++ pumba-web-front/src/components/header/HeaderNavigation.vue
@@ -29,6 +29,6 @@ export default {
   @apply text-dark-blue font-normal 
   text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl
   hover:text-blue cursor-pointer
-  hidden sm:inline-block;
+  hidden 2xs:inline-block;
 }
 </style>
