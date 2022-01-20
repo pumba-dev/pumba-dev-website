20970573b4f5edbe80d45cd910f5dd19f6897778
pumba-dev <dudu32769144@gmail.com>
2022-01-20 18:38:11 -0300

    AboutMe Reponsivity Added

--- pumba-web-front/src/components/about-me/AboutMe.vue
+++ pumba-web-front/src/components/about-me/AboutMe.vue
@@ -15,13 +15,37 @@
 
 <style scoped>
 .about-me-text {
-  @apply w-950 text-gray-blue;
+  @apply text-gray-blue 
+  w-[18.75rem]
+  2xs:w-[21.8rem]
+  xs:w-[22.5rem]
+  sm:w-[30rem]
+  md:w-[40rem]
+  lg:w-[55rem]
+  xl:w-[70rem]
+  2xl:w-[75rem];
 }
 .about-me-title {
-  @apply text-center text-2xl font-bold tracking-wider;
+  @apply text-center font-bold tracking-wide
+  text-[0.8rem]
+  2xs:text-[0.85rem]
+  xs:text-[0.9rem]
+  sm:text-[1rem]
+  md:text-[1.3rem] md:tracking-wider
+  lg:text-[1.5rem]
+  xl:text-[2.0rem]
+  2xl:text-[2rem];
 }
 
 .about-me-subtitle {
-  @apply text-center pt-4 leading-relaxed tracking-wider;
+  @apply text-justify leading-snug tracking-normal 
+  text-xs py-1
+  2xs:text-[0.8rem]
+  xs:text-[0.83rem]
+  sm:text-[0.9rem]
+  md:text-[1.2rem] md:py-2 md:tracking-wide
+  lg:text-[1.4rem] lg:py-3
+  xl:text-[1.7rem] xl:py-4 xl:tracking-wider
+  2xl:text-[1.85rem] 2xl:py-6;
 }
 </style>
