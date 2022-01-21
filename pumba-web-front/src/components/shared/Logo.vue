4506886e8e9776aa74c2490c03d1248f6b96e927
pumba-dev <dudu32769144@gmail.com>
2022-01-21 18:14:00 -0300

    Added link to top in logo

--- pumba-web-front/src/components/shared/Logo.vue
+++ pumba-web-front/src/components/shared/Logo.vue
@@ -3,11 +3,23 @@
     class="logo"
     src="/src/assets/svg/general/logo.svg"
     alt="Pumba Developer Logo"
+    @click="this.scrollUp()"
   />
 </template>
 
+<script>
+export default {
+  methods: {
+    scrollUp() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+  },
+};
+</script>
+
 <style scoped>
 .logo {
-  @apply w-12 sm:w-14 md:w-16 lg:w-20 xl:w-[5.5rem] 2xl:w-[6rem];
+  @apply w-12 sm:w-14 md:w-16 lg:w-20 xl:w-[5.5rem] 2xl:w-[6rem]
+  lg:cursor-pointer;
 }
 </style>
