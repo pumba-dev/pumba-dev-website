c23ea955cb94f29987e854e610bf90357774d37f
pumba-dev <pumbadeveloper@gmail.com>
2026-03-28 17:20:26 -0300

    refactor: extract BACKEND_API_URL into a constant for improved readability

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
+
+console.log("BACKEND_API_URL:", BACKEND_API_URL);
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  baseURL: BACKEND_API_URL,
   timeout: 60000, // 1 minuto
   headers: {
     "Content-Type": "application/json",
